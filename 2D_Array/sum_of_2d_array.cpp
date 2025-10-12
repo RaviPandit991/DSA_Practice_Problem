e48d97e0b61b2a7f2eeb9684bb6bb099e1bc1e73
Ravi Pandit <panditravi6189@gmail.com>
2025-10-12 10:06:02 +0530

    Create sum_of_2d_array.cpp

---
+++ 2D_Array/sum_of_2d_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of row : ";
+    cin>>n;
+    int m;
+    cout<<"Enter number of columns : ";
+    cin>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            sum+=arr[i][j];
+        }
+    }
+    cout<<sum;
+}
