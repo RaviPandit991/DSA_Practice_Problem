cbecb76856c880806305d39b049adda2e33f0667
Ravi Pandit <panditravi6189@gmail.com>
2025-10-15 14:31:02 +0530

    Create binary_search.cpp

---
+++ Array/binary_search.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool check=false;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+
+    int l=0,r=n-1;
+    while(l<=r){
+        int m=(l+r)/2;
+        if(arr[m]==target){
+            cout<<"Found : "<<m;
+            check=true;
+            break;
+        }
+        else if(arr[m]<target){
+            l=m+1;
+        }
+        else{
+            r=m-1;
+        }
+    }
+    if(check==false){
+        cout<<"Not found";
+    }
+}
