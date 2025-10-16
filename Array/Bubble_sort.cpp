e61bd7f37e1555827065dc394368e441958f410e
Ravi Pandit <panditravi6189@gmail.com>
2025-10-16 10:54:18 +0530

    Update Bubble_sort.cpp

--- Array/Bubble_sort.cpp
+++ Array/Bubble_sort.cpp
@@ -9,7 +9,7 @@ int main(){
     }
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(arr[j]>arr[i]){
+            if(arr[j]<arr[i]){
                 swap(arr[i],arr[j]);
             }
         }
