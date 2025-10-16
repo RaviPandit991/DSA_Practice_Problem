11187de6822607a7441019aec0bc82e90c7bc113
Ravi Pandit <panditravi6189@gmail.com>
2025-10-16 20:23:20 +0530

    Create Selection_sort.cpp

---
+++ Array/Selection_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        int idx=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[idx]){
+                idx=j;
+            }
+        }
+        swap(arr[i],arr[idx]);
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
