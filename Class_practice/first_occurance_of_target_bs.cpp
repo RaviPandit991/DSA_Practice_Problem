7c77ca9d2a41bb8a3643bfc8c102077f9d0ae1ca
Ravi Pandit <panditravi6189@gmail.com>
2025-10-22 13:43:52 +0530

    Create first_occurance_of_target_bs.cpp

---
+++ Class_practice/first_occurance_of_target_bs.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int found;
+    int target =2;
+    int arr[9]={1,1,2,2,3,3,4,5,6};
+    int i=0,j=8;
+    while(i<=j){
+        int mid=(i+j)/2;
+        if(arr[mid]==target){
+            found=mid;
+            j=mid-1;
+        }else if(arr[mid]>target){
+            j=mid-1;
+        }else{
+            i=mid+1;
+        }
+    }
+    cout<<found;
+}
