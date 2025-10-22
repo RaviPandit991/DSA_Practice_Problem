64a3bdd580ca5b7603289e25f0552481cdbf0d33
Ravi Pandit <panditravi6189@gmail.com>
2025-10-22 13:43:59 +0530

    Update insertion_sort.cpp

--- Array/insertion_sort.cpp
+++ Array/insertion_sort.cpp
@@ -8,13 +8,13 @@ int main(){
         cin>>arr[i];
     }
     for(int i=1;i<n;i++){
-        for(int j=i;j>0;j--){
+        for(int j=i;j>=0;j--){
             if(arr[j]<arr[j-1]){
                 swap(arr[j],arr[j-1]);
             }
         }
     }
     for(int i=0;i<n;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<endl;
     }
 }
