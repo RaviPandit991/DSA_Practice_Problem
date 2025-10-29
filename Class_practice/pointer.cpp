64966c172a4272a490a6f09609ec2561f7621c34
Ravi Pandit <panditravi6189@gmail.com>
2025-10-29 11:48:54 +0530

    Create pointer.cpp

---
+++ Class_practice/pointer.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class student{
+    private: 
+        int age,rollno;
+    public:
+        void setvalue(int age,int rollno){
+            this->age=age;
+            this->rollno=rollno;
+        }
+        void getvalue(){
+            cout<<age<<" "<<rollno<<endl;
+        }
+};
+int main(){
+    int size = 5;
+    student *s=new student[size];
+
+    for(int i=0;i<size;i++){
+        int a,b;
+        cin>>a>>b;
+        s[i].setvalue(a,b);
+    }
+
+    for(int i=0;i<size;i++){
+        s[i].getvalue();
+    }
+}
