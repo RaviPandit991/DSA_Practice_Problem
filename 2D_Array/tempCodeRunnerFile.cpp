9f0d75cf5a68545bf2ace3fbf8d1723cf4add611
Ravi Pandit <panditravi6189@gmail.com>
2025-11-04 12:10:03 +0530

    Create tempCodeRunnerFile.cpp

---
+++ 2D_Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+ // for(int i=0;i<n;i++){
+    //     int sum=0;
+    //     for(int j=0;j<m;j++){
+    //         sum+=arr[i][j];
+    //     }
+    //     cout<<"Sum of "<<i<<" row : "<<sum<<endl;
+    // }
