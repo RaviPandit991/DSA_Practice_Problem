60d961da632dbfc747a6218e73a489a37ee25cc9
Ravi Pandit <panditravi6189@gmail.com>
2025-12-05 12:01:16 +0530

    Create basic.cpp

---
+++ Tree/basic.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+int main(){
+    Node* root = new Node(1);
+    Node* leftNode = new Node(2);
+    Node* rightNode = new Node(3);
+    Node* leftNode1 = new Node(4);
+    Node* rightNode1 = new Node(5);
+    root->left = leftNode;
+    root->right = rightNode;
+    root->left->left = leftNode1;
+    root->right->right = rightNode1;
+}
+
+
+//using recursion to print pre-order tree;
+// void print(Node* root){
+//     if(root == NULL){
+//         return;
+//     }
+//     print(root->left);
+//     print(root->right);
+// }
