ba1af2542e7ce85e1fd04bd19a82c7575005f4c2
Ravi Pandit <panditravi6189@gmail.com>
2026-02-11 23:19:24 +0530

    Create change_in_string.cpp

---
+++ string/change_in_string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin,str);
+    cout<<str<<endl;
+    for(int i=0;str[i]!='\0';i++){
+        if(i%2==0){
+            str[i] = 'a';
+        }
+    }
+    cout<<str;
+}
