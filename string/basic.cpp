983fe9e804d7e29ee0de6cdf09045ec05d0c0934
Ravi Pandit <panditravi6189@gmail.com>
2026-02-13 00:56:53 +0530

    Update basic.cpp

--- string/basic.cpp
+++ string/basic.cpp
@@ -1,39 +1,13 @@
 #include <iostream>
-//frequency count
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, m;
-    cout << "Enter number of rows: ";
-    cin >> n;
-    cout << "Enter number of columns: ";
-    cin >> m;
-
-    int arr[n][m];
-    cout << "Enter matrix elements:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-        }
-    }
-    bool arr2[n][m] = {false};
-    cout << "\nElement frequencies:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (!arr2[i][j]) {
-                int current = arr[i][j];
-                int count = 0;
-                for (int x = 0; x < n; x++) {
-                    for (int y = 0; y < m; y++) {
-                        if (arr[x][y] == current) {
-                            count++;
-                            arr2[x][y] = true;
-                        }
-                    }
-                }
-                cout << current << "  " << count << " times"<<endl;
-            }
-        }
-    }
-    return 0;
+	// your code goes here
+    string str;
+    getline(cin,str);
+    int n = str.length();
+    reverse(str.begin()+2,str.begin()+6);
+    cout<<str;
+	return 0;
 }
