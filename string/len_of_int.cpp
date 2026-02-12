6c7e38547e81678f34e934e8833550a6a8099d06
Ravi Pandit <panditravi6189@gmail.com>
2026-02-13 00:56:56 +0530

    Create len_of_int.cpp

---
+++ string/len_of_int.cpp
@@ -0,0 +1,8 @@
+//find the length of int value without using loop
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n = 123455656;
+    string s = to_string(n);
+    cout<<s.length();
+}
