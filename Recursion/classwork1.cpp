add0d190b87a46ede3adb58a55e162f53b70a70a
Ravi Pandit <panditravi6189@gmail.com>
2026-06-13 23:36:39 +0545

    Create classwork1.cpp

---
+++ Recursion/classwork1.cpp
@@ -0,0 +1,15 @@
+// print good morning n number of times
+#include<iostream>
+using namespace std;
+void greeting(int n){
+    if(n==0) return;
+    cout<<"Good Morning "<<endl;
+    greeting(n-1);
+}
+
+int main(){
+    int n;
+    cout<<"Enter the value of N: ";
+    cin>>n;
+    greeting(n);
+}
