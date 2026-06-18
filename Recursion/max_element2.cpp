e792f340c4c5e944d270f7a236e22de81cc9d2ab
Ravi Pandit <panditravi6189@gmail.com>
2026-06-19 00:50:10 +0530

    Create max_element2.cpp

---
+++ Recursion/max_element2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits\stdc++.h>
+using namespace std;
+int display(int arr[], int n, int idx){
+    if(idx==n) return INT_MIN;
+    return max(arr[idx],display(arr,n,idx+1));
+}
+
+int main(){
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<display(arr,n,0);
+}
