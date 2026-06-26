c54bb0ffcde5997a5af29d1cfb267886dd6f1bed
Ravi Pandit <panditravi6189@gmail.com>
2026-06-26 23:14:04 +0530

    Update permutation.cpp

--- Recursion/permutation.cpp
+++ Recursion/permutation.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+void permutation(string ans, string original){
+    if(original==""){
+        cout<<ans<<endl;
+        return;
+    }
+    for(int i=0;i<original.size();i++){
+        char c = original[i];
+        string right=original.substr(i+1);
+        string left=original.substr(0,i);
+        permutation(ans+c,right+left);
+    }
+}
+
 int main(){
-    cout<<"hello";
+    string str = "abc";
+    permutation("",str);   
 }
